583af685cdb0f6268f27e3a0b5d58794ead4bd32
hobbyscripterII <wruomma2@naver.com>
2025-02-22 13:13:43 +0900

    [2025-02-22] UPDATE - 운영 서버 요청 프로토콜 변경

--- src/main/java/com/project/smartfarmapi/cmmn/SwaggerConfig.java
+++ src/main/java/com/project/smartfarmapi/cmmn/SwaggerConfig.java
@@ -1,25 +1,42 @@
 package com.project.smartfarmapi.cmmn;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+	@Value("${spring.config.activate.on-profile}")
+	private String activeProfile;
+	
+	@Value("${swagger.base-url}")
+	private String baseUrl;
+	
 	@Bean
 	public OpenAPI openAPI() {
+		List<Server> servers = new ArrayList<Server>();
+		servers.add(new Server().url(baseUrl));
+		
 		return new OpenAPI()
 				.components(new Components())
-				.info(apiInfo());
+				.info(apiInfo())
+				.servers(servers);
 	}
 	
 	private Info apiInfo() {
 		return new Info()
-				.title("API")
-				.description("API Interface Docs!")
+				.title("SMART FARM API")
+				.description("<strong>SMART FARM API Interface Docs!</strong> (" + activeProfile + " SERVER)")
 				.version("1.0.0");
 	}
 }
--- src/main/java/com/project/smartfarmapi/test/TestController.java
+++ src/main/java/com/project/smartfarmapi/test/TestController.java
@@ -3,7 +3,6 @@ package com.project.smartfarmapi.test;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @Tag(name = "테스트 API", description = "API 호출 테스트 관련 파트")
 public class TestController {
-	@GetMapping({"/", "/test"})
+	@GetMapping("/")
 	@Operation(summary = "API 호출 테스트 기능", description = "")
 	public ResponseEntity<ApiResponse<TestVo>> test() {
 		TestVo vo = new TestVo("API 호출 테스트 완료", now());
